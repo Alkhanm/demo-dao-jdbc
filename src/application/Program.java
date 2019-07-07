a6aeb3eddb185c9cc5b0028205272e8b7ec05def
Alkhanm <Joas7777777@hotmail.com>
2019-07-07 16:41:08 -0300

    Class department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,52 +1,14 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection con = null;
-		Statement st = null;
-
-		try {
-			con = DB.getConnection();
-			
-			st = con.createStatement();
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 		
-			con.setAutoCommit(false);
-			
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-			
-			//int x = 1;
-			//if (x < 2) {
-			//	throw new SQLException("Fake error");
-			//}
-			
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-	
-			con.commit();
-			System.out.println("Rows1 "  + rows1);
-			System.out.println("Rows2 "  + rows2);
-			
-		} catch (SQLException ex) {
-			try {
-				con.rollback();
-				throw new DbException("Transaction rolled back! Caused by: " + ex.getMessage());
-			} catch (SQLException ex1) {
-				throw new DbException("Error trying to rollback! Caused by:" + ex1.getMessage());
-			}
-		} finally {
-			DB.closeStatment(st);
-			DB.closeConnection();
-		}
-
 	}
 }
 
